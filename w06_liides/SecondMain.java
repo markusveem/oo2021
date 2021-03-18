1bc481e11059085975d7593f34f87976a8c7ef09
Markus Veem <markusv@tlu.ee>
2021-03-18 23:49:42 +0200

    Homework

--- w06_liides/SecondMain.java
+++ w06_liides/SecondMain.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class SecondMain {
     
     public static void main(String[] args) {
 
-        blender();
-        Fl();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("vali mis programmi hinnata");
+        System.out.println("B-->blender; Fl --> fruity loops");
+        String program = scan.nextLine();
+
+
+        if (program.equals("B")){
+            blender();
+
+        }else{
+            Fl();
+        }
+
+
+
+        //blender();
+        //Fl();
 
     
         
