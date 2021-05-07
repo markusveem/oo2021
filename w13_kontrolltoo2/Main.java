f28404ba43db464e7b367d37dcdd3ae6cf919e8c
Markus Veem <markusv@tlu.ee>
2021-05-07 13:48:33 +0300

    test changed

---
+++ w13_kontrolltoo2/Main.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+
+    public void filReader() throws FileNotFoundException {
+        
+
+        File file = new File("tekst.txt");
+        Scanner scanner = new Scanner(file);
+    }
+}
